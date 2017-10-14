ab2e1f78f693b5f075e2bed59fd5d972812a3b7c
= <desuko@gmail.com>
2017-10-15 01:16:06 +0200

    re-ordering hierarchy

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('default', function(){
-	return gulp.src('./cidjy.js')
+	return gulp.src('./src/*.js')
 	.pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-	.pipe(gulp.dest('./'));
+	.pipe(gulp.dest('./build'));
 });
